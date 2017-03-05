287937f51654eaca2c0d5c4e4c9db94eb27c5deb
frankChin <frank65501830@gmail.com>
2017-03-05 17:41:56 +0800

    second

--- test.py
+++ test.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.ptt.cc/bbs/Beauty/M.1488596143.A.919.html'
 
+"""
+test branch
+"""
 req2 = requests.get(url)
 soup2 = BeautifulSoup(req2.text, 'html.parser')
 
